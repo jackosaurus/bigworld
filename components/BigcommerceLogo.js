5e646b64982f7c2122f7a8c0a551c15a5bfa2fd3
Jack Dinh <jack.dinh@bigcommerce.com>
2017-08-10 17:16:19 +1000

    add bigcommercelogo, providerlogo

---
+++ components/BigcommerceLogo.js
@@ -0,0 +1,8 @@
+/* eslint-disable new-cap */
+import React from 'react';
+
+export default () => (
+	<div>
+		<img width="123" src="https://s3.amazonaws.com/www1.bigcommerce.com/assets/mediakit/downloads/BigCommerce-logo-dark.png" />
+	</div>
+);
---
+++ components/ProviderLogo.js
@@ -0,0 +1,8 @@
+/* eslint-disable new-cap */
+import React from 'react';
+
+export default () => (
+	<div>
+		<img src="https://bigpay.integration.zone/images/providers/klarna.png" />
+	</div>
+);
