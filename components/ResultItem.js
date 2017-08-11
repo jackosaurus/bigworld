a1b1213984d27696422e55d2d56dc2c53bf94f6c
Jack Dinh <jack.dinh@bigcommerce.com>
2017-08-11 14:51:35 +1000

    fix broken styles for result items

--- components/ResultItem.js
+++ components/ResultItem.js
@@ -7,23 +7,25 @@ const { string } = React.PropTypes;
 
 function ResultItem({ id, logo, alt, title }) {
     return (
-        <Link href={{ pathname: '/detail', query: { provider: id } }} className="resultItemContainer">
-            <div>
-                <h4>{title}</h4>
-                <div className="resultItem-providerLogo">
-                    <ProviderLogo url={logo} alt={alt} />
-                </div>
-                <div className="providerType">
-                    <span className="label label-primary">Credit</span>
-                    <span className="label label-warning">Credit card</span>
-                    <span className="label label-success">Pay after delivery</span>
-                </div>
-                {title}
-                <div>
-                    <span className="glyphicon glyphicon-grain" />
-                </div>
-            </div>
-        </Link>
+    	<div className="resultItemContainer">
+	        <Link href={{ pathname: '/detail', query: { provider: id } }}>
+	            <div>
+	                <h4>{title}</h4>
+	                <div className="resultItem-providerLogo">
+	                    <ProviderLogo url={logo} alt={alt} />
+	                </div>
+	                <div className="providerType">
+	                    <span className="label label-primary">Credit</span>
+	                    <span className="label label-warning">Credit card</span>
+	                    <span className="label label-success">Pay after delivery</span>
+	                </div>
+	                {title}
+	                <div>
+	                    <span className="glyphicon glyphicon-grain" />
+	                </div>
+	            </div>
+	        </Link>
+        </div>
     );
 }
 
